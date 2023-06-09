b6af558a84007dbe762ffd840f62bf9055a872e9
ShishirShekhar <sspdav02@gmail.com>
2023-06-09 20:26:45 +0530

    frontend for manufacturer and transporter

--- client/src/pages/Manufacturer.jsx
+++ client/src/pages/Manufacturer.jsx
@@ -1,7 +1,131 @@
-import React from "react";
+import { useState } from "react";
+import axios from "axios";
+
+import getCurrentLocation from "../utils/currentLocation";
 
 const Manufacturer = () => {
-  return <div>Manufacturer</div>;
+  const [orderId, setOrderId] = useState("");
+  const [to, setTo] = useState("");
+  const [from, setFrom] = useState("");
+  const [quantity, setQuantity] = useState("1");
+  const [address, setAddress] = useState("");
+  const [transporter, setTransporter] = useState("t1");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
+
+  const generateOrderId = () => {
+    const characters =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    const length = 8;
+    let result = "";
+    for (let i = 0; i < length; i++) {
+      result += characters.charAt(
+        Math.floor(Math.random() * characters.length)
+      );
+    }
+
+    setOrderId(result);
+  };
+
+  const setLocation = async () => {
+    const { latitude, longitude } = await getCurrentLocation();
+    const response = await axios.get(
+      `https://api.mapbox.com/geocoding/v5/mapbox.places/${longitude},${latitude}.json?access_token=${accessToken}`
+    );
+
+    setAddress(response.data.features[0].place_name);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="flex items-center justify-center h-screen bg text-white text-center">
+      <div className="w-full sm:max-w-md">
+        <div className="form_container">
+          <h2 className="form_title">Manufacturer</h2>
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="orderId">Order Id:</label>
+            <input
+              type="text"
+              id="orderId"
+              name="orderId"
+              placeholder="Order Id"
+              value={orderId}
+              onClick={generateOrderId}
+              required
+            />
+
+            <label htmlFor="to">To:</label>
+            <input
+              type="text"
+              id="to"
+              name="to"
+              placeholder="To"
+              value={to}
+              onChange={(e) => setTo(e.target.value)}
+              required
+            />
+
+            <label htmlFor="from">From:</label>
+            <input
+              type="text"
+              id="from"
+              name="from"
+              placeholder="From"
+              value={from}
+              onChange={(e) => setFrom(e.target.value)}
+              required
+            />
+
+            <label htmlFor="quantity">Quantity:</label>
+            <select
+              id="quantity"
+              name="quantity"
+              value={quantity}
+              onChange={(e) => setQuantity(e.target.value)}
+            >
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+            </select>
+
+            <label htmlFor="address">Address:</label>
+            <input
+              type="text"
+              id="address"
+              name="address"
+              placeholder="Address"
+              value={address}
+              onClick={setLocation}
+              required
+            />
+
+            <label htmlFor="transporter">Transporter:</label>
+            <select
+              id="transporter"
+              name="transporter"
+              value={transporter}
+              onChange={(e) => setTransporter(e.target.value)}
+            >
+              <option value="t1">t1</option>
+              <option value="t2">t2</option>
+            </select>
+
+            {errorMessage && (
+              <p className="text-white mb-2">{errorMessage}!!</p>
+            )}
+
+            <button type="submit" className="submit">
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Manufacturer;
--- client/src/pages/Transporter.jsx
+++ client/src/pages/Transporter.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import { useState } from "react";
 
 const Transporter = () => {
-  return (
-    <div>Transporter</div>
-  )
-}
+  const [orderId, setOrderId] = useState("1");
+  const [price, setPrice] = useState("1");
+  const [errorMessage, setErrorMessage] = useState("");
 
-export default Transporter
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="flex items-center justify-center h-screen bg text-white text-center">
+      <div className="w-full sm:max-w-md">
+        <div className="form_container">
+          <h2 className="form_title">Transporter</h2>
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="orderId">Order Id:</label>
+            <select
+              id="orderId"
+              name="orderId"
+              value={orderId}
+              onChange={(e) => setOrderId(e.target.value)}
+            >
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+            </select>
+
+            <label htmlFor="price">Price:</label>
+            <input
+              type="number"
+              id="price"
+              name="price"
+              placeholder="Price"
+              value={price}
+              onChange={(e) => setPrice(e.target.value)}
+              required
+            />
+
+            {errorMessage && (
+              <p className="text-white mb-2">{errorMessage}!!</p>
+            )}
+
+            <button type="submit" className="submit">
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Transporter;
